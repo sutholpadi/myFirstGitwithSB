db024018ee74f1203e060a232a6e064b416e9dbf
root <root@tholpadi-Lenovo-ideapad-100-14IBY>
2018-04-19 18:35:29 -0500

    test change

--- RESTSB/testRESTfulSB/src/main/java/hello/GreetingController.java
+++ RESTSB/testRESTfulSB/src/main/java/hello/GreetingController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
-
+//test comment to merge.
 @RestController
 @RequestMapping(value="/RESTfulSP")
 public class GreetingController {
